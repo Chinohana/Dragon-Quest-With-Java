d62af57d5effcc41a6807b095e70c072531db529
Chinohana <chinohana@outlook.com>
2023-04-01 02:39:06 +0800

    add Story database

---
+++ src/Story/Story.java
@@ -0,0 +1,27 @@
+package Story;
+public class Story {
+    public static void Story01() {
+        System.out.print("---------------------血花Chinohana制作---------------------\n");
+        System.out.print("公告栏：本游戏为血花Chinohana制作的Java语言版本，可能有一定bug和功能不完善，这是因为作者对Java语言不熟悉，非常抱歉。\n如果遇到bug，请发邮件给作者：chinohana@outlook.com。游戏的源代码上传到了github，我的github账号是https://github.com/Chinohana\n");
+        System.out.print("你好，欢迎来到这款游戏，这是一个有剧情的猜数字游戏。\n");
+        System.out.print("系统：让我们开始吧！！\n");
+        System.out.print("系统:按enter开始\n");
+    }
+    public static void Story02() {
+        System.out.print("很久很久以前\n有一个王国\n这个王国总是被恶龙侵扰\n派去的勇者最后都不知去向\n而你作为王国的最后希望\n去调查这一切的真相。\n");
+        System.out.print("系统：请你告诉我你是什么名字，勇士\n");
+    }
+
+    public static void Story03(String name) {
+        System.out.print("英雄"+name+"\n");
+        System.out.print("骑上最快的马\n带着大家的希望\n从城堡里出发\n战胜怪兽来袭\n获得12金币\n无数伤痕见证\n你慢慢升级\n");
+        System.out.print("按enter继续");
+    }
+
+    public static void Story04() {
+        System.out.print("偏远美丽村庄\n打开所有宝箱\n一路风霜伴随指引前路的圣月光。。。\n");
+        System.out.print("按enter继续");
+    }
+
+}
+
